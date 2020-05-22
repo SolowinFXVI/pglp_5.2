fe33c195076900cd19a82660ce8ac1ddf097fc1e
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-05-23 00:09:57 +0200

    Updated test for derby and general DB tests

--- src/test/java/org/uvsq21400579/AppTest.java
+++ src/test/java/org/uvsq21400579/AppTest.java
@@ -1,20 +1,75 @@
 package org.uvsq21400579;
 
 import static org.junit.Assert.assertEquals;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.uvsq21400579.Employee.Builder;
+import java.sql.Connection;
 
 public class AppTest {
+
+    /**
+     * Creates the embedded database required by some tests
+     */
+    @BeforeClass
+    public static void initializeEmbeddedDB(){
+        String createEmployeeTableString = "CREATE TABLE \"EMPLOYEE\"(NAME VARCHAR (128) NOT NULL, SURNAME VARCHAR(128) NOT NULL, PRIMARY KEY (NAME) , \"FUNCTION\" VARCHAR(128), PHONE VARCHAR(128), BIRTHDATE VARCHAR(128))";
+        String createGroupTableString = "CREATE TABLE \"GROUP\"(NAME VARCHAR (128) PRIMARY KEY NOT NULL)";
+        String createGroupDirectoryTableString = "CREATE TABLE GROUPDIRECTORY(NAME VARCHAR(128) PRIMARY KEY, FOREIGN KEY (NAME) REFERENCES \"GROUP\"(NAME))";
+        String createDirectoryTableString = "CREATE TABLE DIRECTORY(NAME VARCHAR(128) PRIMARY KEY, FOREIGN KEY (NAME) REFERENCES EMPLOYEE(NAME))";
+        String createIsEmployeeTableString = "CREATE TABLE ISEMPLOYEE(GROUPNAME VARCHAR(128), NAME VARCHAR(128), PRIMARY KEY (GROUPNAME, NAME), FOREIGN KEY (GROUPNAME) REFERENCES \"GROUP\"(NAME),FOREIGN KEY (NAME) REFERENCES EMPLOYEE(NAME))";
+        String createIsGroupTableString = "CREATE TABLE ISGROUP(GROUPNAME VARCHAR(128), NAME VARCHAR(128), PRIMARY KEY (GROUPNAME, NAME), FOREIGN KEY (GROUPNAME) REFERENCES \"GROUP\"(NAME), FOREIGN KEY (NAME) REFERENCES \"GROUP\"(NAME))";
+        String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+        String protocol = "jdbc:derby:";
+        try{
+            Class.forName(driver).newInstance();
+            Connection connection = DriverManager.getConnection(protocol + "derbyDB;create=true");
+            Statement statement = connection.createStatement();
+
+            statement.execute(createEmployeeTableString);
+            statement.execute(createGroupTableString);
+            statement.execute(createGroupDirectoryTableString);
+            statement.execute(createDirectoryTableString);
+            statement.execute(createIsEmployeeTableString);
+            statement.execute(createIsGroupTableString);
+
+            connection.close();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Drops tables from the database so that between reruns the database is in a default state
+     * @throws SQLException
+     */
+    @AfterClass
+    public static void shutdownEmbeddedDB() throws SQLException {
+        String cleanupEmployeeString = "DROP TABLE \"EMPLOYEE\"";
+        String cleanupGroupString = "DROP TABLE \"GROUP\"";
+        String cleanupGroupDirectoryString = "DROP TABLE GROUPDIRECTORY";
+        String cleanupDirectoryString = "DROP TABLE DIRECTORY";
+        String cleanupIsEmployeeString = "DROP TABLE ISEMPLOYEE";
+        String cleanupIsGroupString = "DROP TABLE ISGROUP";
+
+        Connection connection = DriverManager.getConnection("jdbc:derby:derbyDB");
+        Statement statement = connection.createStatement();
+        statement.execute(cleanupIsGroupString);
+        statement.execute(cleanupIsEmployeeString);
+        statement.execute(cleanupDirectoryString);
+        statement.execute(cleanupGroupDirectoryString);
+        statement.execute(cleanupEmployeeString);
+        statement.execute(cleanupGroupString);
+        connection.close();
+    }
+
     @Test
     public void testEmployee() {
         Employee employee = new Employee.Builder("Dylan", "Bob", "Singer").build();
@@ -24,8 +79,8 @@ public class AppTest {
     @Test
     public void testEmployeeDate() {
         LocalDate localDate = LocalDate.now();
-        Employee employee = new Employee.Builder("Dylan", "Bob", "Singer").updateBirthDate(localDate).build();
-        assertEquals(LocalDate.now(), employee.getLocalDate());
+        Employee employee = new Builder("Dylan", "Bob", "Singer").updateBirthDate(localDate).build();
+        assertEquals(LocalDate.now(), employee.getBirthDate());
     }
 
     @Test
@@ -83,6 +138,9 @@ public class AppTest {
     }
 
 
+    /**
+     * Test for Directory, adding employees, teams and groups
+     */
     @Test
     public void testDirectory() {
         List<String> tmp = new ArrayList<>();
@@ -105,85 +163,62 @@ public class AppTest {
         }
     }
 
+    /**
+     * Test for Employee, creating, finding and deleting in derby
+     */
     @Test
-    public void serializationWriteReadTest() {
+    public void testEmployeeDB(){
         List<String> tmp = new ArrayList<>();
         Employee employee = new Employee.Builder("Dylan", "Bob", "Singer").updatePhoneList(tmp).build();
         Employee employee2 = new Employee.Builder("Hendrix", "Jimmy", "GuitarHero").updatePhoneList(tmp).build();
-        Directory directory = Directory.getInstance();
-        Group group_1 = new Group("TEST_1");
-        Group group_2 = new Group("TEST_2");
-        group_1.addMember(employee);
-        group_1.addMember(employee2);
-        group_2.addMember(employee);
-        directory.addTeam(employee);
-        directory.addTeam(new Group("group_1"));
-        directory.addTeam(new Group("group_2"));
-        directory.addTeam(group_1);
-        directory.addTeam(group_2);
-        for (Team team : directory) {
-            team.printName();
-        }
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new BufferedOutputStream(
-            new FileOutputStream("./src/test/java/org/uvsq21400579/dataFile")))) {
-            outputStream.writeObject(directory);
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+        EmployeeDAO employeeDAO = new EmployeeDAO();
+        employeeDAO.create(employee);
+        employeeDAO.create(employee2);
+        Employee result1 = employeeDAO.find("Dylan");
+        Employee result2 = employeeDAO.find("Hendrix");
+        employeeDAO.delete("Dylan");
+        employeeDAO.delete("Hendrix");
+        assertEquals(result1.getName(),employee.getName());
+        assertEquals(result1.getSurname(),employee.getSurname());
+        assertEquals(result1.getFunction(), employee.getFunction());
+        //PHONE
+        assertEquals(result1.getBirthDate(),employee.getBirthDate());
 
-        try (ObjectInputStream inputStream = new ObjectInputStream(new BufferedInputStream(
-            new FileInputStream("./src/test/java/org/uvsq21400579/dataFile")))) {
-            Directory test = (Directory) inputStream.readObject();
-            System.out.println("TEST");
-            for (Team team : test) {
-                team.printName();
-            }
-        } catch (IOException | ClassNotFoundException exception) {
-            exception.printStackTrace();
-        }
+        assertEquals(result2.getName(), employee2.getName());
+        assertEquals(result2.getSurname(), employee2.getSurname());
+        assertEquals(result2.getFunction(), employee2.getFunction());
+        //PHONE
+        assertEquals(result2.getBirthDate(), employee2.getBirthDate());
     }
 
+    /**
+     * Test for Group, creating, finding and deleting in derby
+     */
     @Test
-    public void DirectoryDAOTest() {
+    public void testGroupDB(){
         List<String> tmp = new ArrayList<>();
         Employee employee = new Employee.Builder("Dylan", "Bob", "Singer").updatePhoneList(tmp).build();
-        Employee employee2 = new Employee.Builder("Hendrix", "Jimmy", "GuitarHero").updatePhoneList(tmp).build();
-        DAO<Directory> dao = DAOFactory.getDirectoryDAO();
-        Directory directory = Directory.getInstance();
-        Group group_1 = new Group("group_1");
-        Group group_2 = new Group("group_2");
-        group_1.addMember(employee);
-        group_1.addMember(employee2);
-        group_2.addMember(employee2);
-        directory.addTeam(group_1);
-        directory.addTeam(group_2);
-        dao.create(directory);
-        Directory test = (Directory) dao.find("directory");
-        dao.delete("directory");
+        Group group1 = new Group("Test");
+        group1.addMember(employee);
+        GroupDAO groupDAO = new GroupDAO();
+        groupDAO.create(group1);
+        Group result1 = groupDAO.find("Dylan");
+        groupDAO.delete("Dylan");
     }
 
+    /**
+     * Test for Directory, creating, finding and deleting in derby
+     */
     @Test
-    public void EmployeeDAOTest() {
+    public void testDirectoryDB(){
         List<String> tmp = new ArrayList<>();
         Employee employee = new Employee.Builder("Dylan", "Bob", "Singer").updatePhoneList(tmp).build();
-        DAO<Employee> dao = DAOFactory.getEmployeeDAO();
-        dao.create(employee);
-        Employee test = (Employee) dao.find("employee");
-        dao.delete("employee");
-    }
-
-    @Test
-    public void GroupDAOTest(){
-        List<String> tmp = new ArrayList<>();
-        Employee employee = new Employee.Builder("Dylan", "Bob", "Singer").updatePhoneList(tmp).build();
-        Employee employee2 = new Employee.Builder("Hendrix", "Jimmy", "GuitarHero").updatePhoneList(tmp).build();
-        DAO<Group> dao = DAOFactory.getGroupDAO();
-        Group group = new Group("group_1");
-        group.addMember(employee);
-        group.addMember(employee2);
-        dao.create(group);
-        Group test = (Group) dao.find("group");
-        dao.delete("group");
+        Directory directory1 = Directory.getInstance();
+        directory1.addTeam(employee);
+        DirectoryDAO directoryDAO = new DirectoryDAO();
+        directoryDAO.create(directory1);
+        Directory result1 = directoryDAO.find("Dylan");
+        directoryDAO.delete("Dylan");
     }
 
 }
